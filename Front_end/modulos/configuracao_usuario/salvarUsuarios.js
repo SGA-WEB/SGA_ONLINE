1fade08cc1bcc80cbd4eeaf60b6726880bb6999f
Matheus Henrique Nunes <matheushnunes2005@gmail.com>
2025-03-30 20:21:30 -0300

    Update salvarUsuarios.js

--- Front_end/modulos/configuracao_usuario/salvarUsuarios.js
+++ Front_end/modulos/configuracao_usuario/salvarUsuarios.js
@@ -1,9 +1,10 @@
 // Versão 100% compatível com seu HTML
 export default function salvarUsuario (dado) {
     'use strict';
+    dado = dado[0]
     console.log(dado)
 
-    document.querySelector("#campo_editar_nome").value = dado[0].nome
-    document.querySelector("#campo_editar_email").value = dado[0].email
+    document.querySelector("#campo_editar_nome").value = dado.nome
+    document.querySelector("#campo_editar_email").value = dado.email
     
 };
