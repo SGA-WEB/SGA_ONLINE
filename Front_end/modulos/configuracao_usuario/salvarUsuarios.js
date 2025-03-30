be63a1bf5261cfeb5575419046aac0123c2e70a2
renatakaren <renatakarenr@gmail.com>
2025-03-30 20:25:16 -0300

    Update salvarUsuarios.js

--- Front_end/modulos/configuracao_usuario/salvarUsuarios.js
+++ Front_end/modulos/configuracao_usuario/salvarUsuarios.js
@@ -1,9 +1,10 @@
-// Versão 100% compatível com seu HTML
 export default function salvarUsuario (dado) {
     'use strict';
     console.log(dado)
 
     document.querySelector("#campo_editar_nome").value = dado[0].nome
     document.querySelector("#campo_editar_email").value = dado[0].email
-    
+    document.querySelector("#campo_editar_fone").value = dado[0].tel
+    document.querySelector("#senha_usuario").value = dado[0].password
+
 };
