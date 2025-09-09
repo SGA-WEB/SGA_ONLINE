d21c750179743f1fa7c40fcedd2b190b41259116
Matheus Henrique Nunes <matheushnunes2005@gmail.com>
2025-09-09 17:32:52 -0300

    Update cadastro_contato.js

--- Front_end/modulos/contato/cadastro_contato/cadastro_contato.js
+++ Front_end/modulos/contato/cadastro_contato/cadastro_contato.js
@@ -6,33 +6,31 @@ import buscarDados from "../../../scripts/buscarDados.js";
 import { popup_carregando } from "../../../scripts/popup.js";
 
 export default async function cadastro_contato() {
-    let cont = 0
+    let dados = {}
+    let arrCategorias = []
+
     popup_carregando()
 
+    async function chamarFuncoes() {
+        await criarDados()
+        dataAtual()
+        btnsProximoEVoltar()
+        addListenerBtns()
+        select2("100%")
+        inserirDadosNaTela("Cadastro de contato")
+    }
 
-    let contatos = await buscarDados('contato');
-    let contatosComMaiorId = contatos.reduce((prev, curr) => {
-        return prev.id_contato > curr.id_contato ? prev : curr;
-    })
-    let nextIdContato = contatosComMaiorId.id_contato + 1;
-    document.querySelector(".codigo_id").innerHTML = nextIdContato;
+    carregarConteudo("contato/cadastro_contato/criar_contato/criar_contato.html", document.querySelector(".modulo"), false, chamarFuncoes); // É passada a função chamarFuncoes para que os botões sejam ativados e os dados sejam inseridos novamente
 
     // Mudar de tela ao clicar no menu superior da tela de contato:
     let links_nav = document.querySelectorAll(".link_nav") // seleciona todos os links do menu superior
-    if (cont == 0) {
-        links_nav[0].classList.add("link_nav_selecionado") // Adiciona a classe ao primeiro link assim que o modulo for carregado
-    }
+    links_nav[0].classList.add("link_nav_selecionado") // Adiciona a classe ao primeiro link assim que o modulo for carregado
 
     document.querySelectorAll('.link_nav').forEach(link => { // Seleciona todos os links
         link.addEventListener("click", (e) => {
             estilo_nav(e.target);
         });
     });
-
-    dataAtual()
-    select2("100%")
-    btnsProximoEVoltar()
-    addListenerBtns()
 
     fecharMenu(document.querySelector(".modulo").offsetWidth, 584)
     window.addEventListener('resize', (e) => {
@@ -49,7 +47,6 @@ export default async function cadastro_contato() {
         document.querySelector("form").addEventListener("submit", salvarDadosNoBanco)
     }
 
-
     function estilo_nav(e) {
         let link = e
         if (e == "voltar_contatos") {
@@ -65,20 +62,7 @@ export default async function cadastro_contato() {
         mudarDeAba(link.id)
     }
 
-    let dados = {}
-    let arrCategorias = []
-
     function mudarDeAba(link) {
-        let form = document.querySelector("form")
-        let dadosFomr = Object.fromEntries(new FormData(form))
-        let elementCategorias = document.getElementsByName("categorias")
-        for (let categoria in elementCategorias) {
-            if (elementCategorias[categoria].checked) {
-                arrCategorias.push(elementCategorias[categoria].value)
-            }
-        }
-        dados = { ...dados, ...dadosFomr }
-        dados.categorias = arrCategorias
         inserirDadosNaTela("Cadastro de contato")
         switch (link) {
             case "link_contato":
@@ -91,11 +75,24 @@ export default async function cadastro_contato() {
         }
     }
 
-    function chamarFuncoes() {
-        inserirDadosNaTela("Cadastro de contato")
-        btnsProximoEVoltar()
-        addListenerBtns()
-        select2("100%")
+    async function criarDados() {
+        console.log(dados)
+        let contatos = await buscarDados('contato');
+        let contatosComMaiorId = contatos.reduce((prev, curr) => {
+            return prev.id_contato > curr.id_contato ? prev : curr;
+        })
+        dados.id_contato = contatosComMaiorId.id_contato + 1;
+
+        let form = document.querySelector("form")
+        let dadosFomr = Object.fromEntries(new FormData(form))
+        let elementCategorias = document.getElementsByName("categorias")
+        for (let categoria in elementCategorias) {
+            if (elementCategorias[categoria].checked) {
+                arrCategorias.push(elementCategorias[categoria].value)
+            }
+        }
+        dados = { ...dados, ...dadosFomr }
+        dados.categorias = arrCategorias
     }
 
     function btnsProximoEVoltar() {
@@ -139,7 +136,7 @@ export default async function cadastro_contato() {
                 }
             }
             let data = new Date()
-            document.querySelector(".codigo_id").textContent = nextIdContato
+            document.querySelector(".codigo_id").textContent = dados.id_contato
             document.querySelector(".data_cadastro").textContent = formatarData(data.toISOString())
             document.querySelector("#nome_razao_social").value = dados.nome_razao_social
             document.querySelector("#nome_fantasia").value = dados.nome_fantasia
