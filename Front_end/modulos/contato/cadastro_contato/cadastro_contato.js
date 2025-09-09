54743c7d6696a6f34cf5e4ae427ca01c1eb78fc6
Matheus Henrique Nunes <matheushnunes2005@gmail.com>
2025-09-08 23:54:35 -0300

    Atualização salvar contato.

--- Front_end/modulos/contato/cadastro_contato/cadastro_contato.js
+++ Front_end/modulos/contato/cadastro_contato/cadastro_contato.js
@@ -42,13 +42,11 @@ export default async function cadastro_contato() {
     })
 
     function addListenerBtns() {
-        document.querySelector(".btn_cancelar").addEventListener("click", () => {
+        document.querySelector(".btn_cancelar").addEventListener("click", (e) => {
+            e.preventDefault()
             carregarConteudo('contato/contato.html', document.querySelector('.principal'), false)
         })
-        document.querySelector(".btn_salvar").addEventListener("click", () => {
-            let data = new Date()
-            salvarNovosDadosDaTelaNoLocalStorage({ data_cadastro: data.toISOString(), id_contato: nextIdContato }, "Cadastro de contato")
-        });
+        document.querySelector("form").addEventListener("submit", salvarDadosNoBanco)
     }
 
 
@@ -115,7 +113,6 @@ export default async function cadastro_contato() {
     }
 
     function inserirDadosNaTela(tituloTela) {
-        console.log(dados)
         if (document.querySelector(".h2_titulo").textContent == tituloTela) { // Verifica se a tela é de visualização
             if (dados.tipo_pessoa === "JURÍDICA") {
                 document.querySelector("#contato_juridico").checked = true
@@ -130,7 +127,6 @@ export default async function cadastro_contato() {
             }
 
             let categorias = dados.categorias
-            console.log(categorias)
             for (let i = 0; i < categorias.length; i++) {
                 if (categorias[i] === "CLIENTE") {
                     document.querySelector("#cliente").checked = true
@@ -160,13 +156,155 @@ export default async function cadastro_contato() {
             document.querySelector("#observacao").value = dados.observacao
         }
         else if (document.querySelector(".h2_titulo").textContent.includes("Endereço")) {
-            document.querySelector("#caixa_postal_principal").value = dados.cep
-            document.querySelector("#pais_principal").value = dados.pais
-            document.querySelector("#estado_principal").value = dados.estado
-            document.querySelector("#municipio_principal").value = dados.municipio
-            document.querySelector("#endereco_principal").value = dados.endereco
-            document.querySelector("#referencia_principal").value = dados.ponto_referencia
-            document.querySelector("#setor_principal").value = dados.setor
+            document.querySelector("#caixa_postal").value = dados.caixa_postal || ""
+            document.querySelector("#pais").value = dados.pais || ""
+            document.querySelector("#estado").value = dados.estado || ""
+            document.querySelector("#municipio").value = dados.municipio || ""
+            document.querySelector("#endereco").value = dados.endereco || ""
+            document.querySelector("#referencia").value = dados.referencia || ""
+            document.querySelector("#setor").value = dados.setor || ""
+        }
+    }
+
+    async function salvarDadosNoBanco(e) {
+        e.preventDefault() // Evita o envio padrão do formulário
+        // Validação básica
+        console.log(dados)
+        if (!dados.razao_social || !dados.fone1 || dados.categorias[0] == "") {
+            // Se algum campo obrigatório estiver vazio, adiciona a classe de erro e foca no campo
+            if (document.querySelector(".h2_titulo").textContent != "Editar contato") {
+                estilo_nav(document.querySelector("#link_contato"))
+            }
+            let nome_razao_social = document.querySelector("#nome_razao_social")
+            let fone1 = document.querySelector("#fone1")
+            setTimeout(() => {
+                console.log(document.querySelector("form"))
+            }, 200);
+            if (dados.categorias[0] == "") {
+                let container_checkbox = document.querySelector(".container_checkbox")
+                container_checkbox.classList.add("border_red")
+                container_checkbox.addEventListener("click", () => {
+                    container_checkbox.classList.remove("border_red")
+                })
+            }
+
+            if (!dados.fone1) {
+                fone1.focus()
+                fone1.classList.add("border_red")
+                fone1.addEventListener("input", () => {
+                    fone1.classList.remove("border_red")
+                })
+            }
+
+            if (!dados.razao_social) {
+                nome_razao_social.focus()
+                nome_razao_social.classList.add("border_red")
+                nome_razao_social.addEventListener("input", () => {
+                    nome_razao_social.classList.remove("border_red")
+                })
+            }
+
+            popup_erro("Campos obrigatórios faltando.");
+            return;
+        }
+
+        // Inserir os dados no banco de dados:
+        try {
+            popup_carregando()
+            const response = await fetch(`http://localhost:3000/api/contato/${id_contato}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dados)
+            });
+
+            const resultado = await response.json();
+
+            if (!response.ok) {
+                popup_erro(`Erro: ${resultado.error}`);
+                return;
+            }
+
+        } catch (error) {
+            console.error('Erro:', error);
+            popup_erro('Erro ao atualizar contato.');
+        }
+
+        // Atualizar as categorias do contato:
+        dados.categorias = dados.categorias.map((categoria) => {
+            // A API espera receber somente o ID da categoria
+            // Então, convertemos o nome da categoria para o ID correspondente no banco de dados
+            if (categoria === "CLIENTE") {
+                return 1
+            } else if (categoria === "FORNECEDOR") {
+                return 2
+            } else if (categoria === "FUNCIONÁRIO") {
+                return 3
+            }
+        })
+
+        try {
+            const response = await fetch(`http://localhost:3000/api/contato/${id_contato}/categorias`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    categorias: dados.categorias
+                })
+            });
+            if (!response.ok) {
+                popup_erro('Erro ao salvar categorias.');
+                const errorData = await response.json();
+                throw new Error(errorData.error || 'Erro ao salvar categorias');
+            }
+
+            const result = await response.json();
+        } catch (error) {
+            popup_erro('Erro ao salvar contato.');
+            console.error('Erro ao salvar:', error);
+        }
+
+        // Atualizar o endereço do contato:
+        const idEndereco = dado.fk_id_endereco; // ID do endereço a ser atualizado
+
+        const dadosEndereco = {
+            cep: localStorage.getItem("cep"),
+            endereco: localStorage.getItem("endereco"),
+            municipio: localStorage.getItem("municipio"),
+            estado: localStorage.getItem("estado"),
+            pais: localStorage.getItem("pais"),
+            ponto_referencia: localStorage.getItem("ponto_referencia"),
+            setor: localStorage.getItem("setor")
+        };
+
+        try {
+            const response = await fetch(`http://localhost:3000/api/endereco/${idEndereco}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(dadosEndereco)
+            });
+            const data = await response.json();
+
+            if (!response.ok) {
+                popup_erro('Erro ao atualizar endereço.');
+            }
+
+            let links_nav = document.querySelectorAll(".link_nav") // Seleciona todos os links do nav
+            links_nav.forEach(e => {
+                e.classList.remove("link_nav_selecionado") // desmarca todos
+            })
+            popup_carregando(true)
+            popup_aviso("Contato atualizado com sucesso!")
+            carregarConteudo(caminho, elementoPai, false, funcao, dadosCompletos)
+            localStorage.clear()
+            return data.endereco;
+        } catch (error) {
+            console.error('Erro:', error);
+            throw error;
         }
     }
 
