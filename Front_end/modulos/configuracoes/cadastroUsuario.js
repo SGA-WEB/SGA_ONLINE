0f132ba63297f97ca7b2165556d1ee2157763070
Guilherme <162363303+Sh1roHebi@users.noreply.github.com>
2025-10-14 21:44:41 -0300

    Tratativa dos campos

--- Front_end/modulos/configuracoes/cadastroUsuario.js
+++ Front_end/modulos/configuracoes/cadastroUsuario.js
@@ -2,22 +2,37 @@ import { popup_erro, popup_aviso } from "../../scripts/popup.js";
 
 export function cadastroUsuario() {
     'use strict';
+    let telefone = document.querySelector("#fone")
+
+    const mascaraTelefone = {
+        mask: [ 
+            {
+                mask: '(00) 00000-0000'
+            }
+        ]
+    };
+
+    // Aplique as máscaras aos elementos
+    if (telefone) {
+        const fone1Mask = IMask(telefone, mascaraTelefone);
+    }
+
 
     document.querySelector("#formulario_usuario").addEventListener("submit", async (e) => {
+        console.log("bla bla")
         e.preventDefault()
         let nome = document.querySelector("#digite-nome").value
         let senha = document.querySelector("#senha").value
         let confirmesenha = document.querySelector("#confirmar-senha").value
         let email = document.querySelector("#email").value
         let confirmeemail = document.querySelector("#confirmar-email").value
-        let telefone = document.querySelector("#fone").value
 
         let dadosOk = true
-        if (senha!=confirmesenha) {
+        if (senha != confirmesenha) {
             popup_erro("As senhas digitadas são diferentes. Tente novamente.")
             dadosOk = false
-        } 
-        if (email!=confirmeemail){
+        }
+        if (email != confirmeemail) {
             popup_erro("Os emails digitados são diferentes. Tente novamente.")
             dadosOk = false
         }
@@ -27,7 +42,7 @@ export function cadastroUsuario() {
             celular: telefone,
             senha: senha
         };
-    
+
         console.log(novoObjeto);
         // Envia os dados atualizados para o backend
         if (dadosOk) {
@@ -39,14 +54,14 @@ export function cadastroUsuario() {
                     },
                     body: JSON.stringify(novoObjeto),
                 });
-        
+
                 if (response.ok) {
                     popup_aviso("Usuário cadastrado com sucesso!")
                     nome = ""
                     senha = ""
                     confirmesenha = ""
                     email = ""
-                    confirmeemail =""
+                    confirmeemail = ""
                     telefone = ""
                 } else {
                     const errorData = await response.json();
