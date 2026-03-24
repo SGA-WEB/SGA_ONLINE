bee8cefe473a921bd01d00876fb21e5ee605c311
Guilherme <guipsouza1377@gmail.com>
2026-03-24 20:07:10 -0300

    Update visualizar_orcamento.js

--- Front_end/modulos/orcamento/visualizar_orcamento/visualizar_orcamento.js
+++ Front_end/modulos/orcamento/visualizar_orcamento/visualizar_orcamento.js
@@ -5,11 +5,14 @@ export default async function visualizar_orcamento (dado) {
     select2("100%")
     document.querySelector(".codigo_id").textContent = dado.id_orcamento
     document.querySelector(".codigo_id").textContent = dado._orcamento
-    // document.querySelector("#cliente").value = dado.cliente_razao_social
-    // document.querySelector("#desconto_total").value = dado.desconto_total_orcamento
-    // document.querySelector("#criado_por_nome").value = dado.criado_por_nome_orcamento
-    // document.querySelector("#status").value = dado.status_orcamento
-    // document.querySelector(".data_criacao").textContent = formatarData(dado.data_criacao)
+    document.querySelector("#cliente").value = dado.cliente_razao_social
+    document.querySelector("#status").value = dado.status
+    document.querySelector("#criado_por").value = dado.criado_por_nome
+    document.querySelector("#subtotal").value = dado.subtotal
+    document.querySelector("#valor_desconto").value = dado.desconto_total
+    document.querySelector("#valor_total").value = dado.valor_total
+    document.querySelector("#observacao").value = dado.observacao
+    document.querySelector(".data_criacao").textContent = formatarData(dado.data_criacao)
     document.querySelector("#btn_voltar_orcamento").addEventListener("click", ()=>{
         carregarConteudo("orcamento/orcamento.html", document.querySelector(".principal"))
     })
