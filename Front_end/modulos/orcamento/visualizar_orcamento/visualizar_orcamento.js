712761f645917cf2aa5ac9a04e86d67e4c275d13
Matheus Henrique <matheushnunes2005@gmail.com>
2026-03-29 17:41:16 -0300

    Visualizar orçamento

--- Front_end/modulos/orcamento/visualizar_orcamento/visualizar_orcamento.js
+++ Front_end/modulos/orcamento/visualizar_orcamento/visualizar_orcamento.js
@@ -1,6 +1,8 @@
 import select2 from "../../../scripts/select.js"
 import { carregarConteudo } from "../../../scripts/javaScript.js"
 import { formatarData } from "../../../scripts/funcionalidades.js"
+import buscarDados from "../../../scripts/buscarDados.js"
+import { carregarDadosNaTabela } from "../../../scripts/carregarDadosNaTabela.js"
 
 export default async function visualizar_orcamento (dado) {
     select2("100%")
@@ -10,6 +12,12 @@ export default async function visualizar_orcamento (dado) {
     document.querySelector("#criado_por_nome").value = dado.criado_por_nome
     document.querySelector("#status").value = dado.status
     document.querySelector(".data_cadastro").textContent = formatarData(dado.data_criacao)
+
+    const orcamento_itens = await buscarDados(`orcamentos/${dado.id_orcamento}/itens`)
+    const orcamento_itens_data = await orcamento_itens.itens
+
+    carregarDadosNaTabela(orcamento_itens_data, ["id_item", "nome_produto", "quantidade", "valor_unitario", "desconto_item", "valor_total_item"], document.querySelector(".tbody"), false, false)
+
     document.querySelector("#btn_voltar_orcamento").addEventListener("click", ()=>{
         carregarConteudo("orcamento/orcamento.html", document.querySelector(".principal"))
     })
