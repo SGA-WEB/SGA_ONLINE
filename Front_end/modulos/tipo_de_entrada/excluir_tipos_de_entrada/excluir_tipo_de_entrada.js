659a5e560f99eb5cce017e1fa943099acdbd2ebd
Matheus Henrique <matheushnunes2005@gmail.com>
2026-05-10 21:46:56 -0300

    Modo card tipos de entrada

--- Front_end/modulos/tipo_de_entrada/excluir_tipos_de_entrada/excluir_tipo_de_entrada.js
+++ Front_end/modulos/tipo_de_entrada/excluir_tipos_de_entrada/excluir_tipo_de_entrada.js
@@ -1,6 +1,4 @@
-import { popup, popup_aviso, popup_carregando, popup_erro, popup_confirmar, popup_confirmar_exclusao } from "../../../scripts/popup.js";
-import buscarDados from "../../../scripts/buscarDados.js"; // Importa a função que busca os dados da tabela
-import { carregarDadosNaTabela } from "../../../scripts/carregarDadosNaTabela.js";
+import { popup_aviso, popup_carregando, popup_erro, popup_confirmar_exclusao } from "../../../scripts/popup.js";
 
 export default async function excluir_tipos_de_entrada(dado, callbackFunction, ...param) {
     let confirmacao = await popup_confirmar_exclusao(`Tem certeza que deseja excluir o tipo de entrada ${dado.id_tipo_de_entrada} - ${dado.descricao}?`)
@@ -11,15 +9,13 @@ export default async function excluir_tipos_de_entrada(dado, callbackFunction, .
             const response = await fetch(`http://localhost:3000/tipos_entrada/${dado.id_tipo_de_entrada}`, {
                 method: 'DELETE'
             });
-            const data = await response.json();
             popup_carregando(true)
             if (response.ok) {
                 popup_aviso(`Tipo de entrada ${dado.descricao} excluído com sucesso!`)
+                document.querySelector(`#tr_${dado.id_tipo_de_entrada}`)?.remove();
                 if (callbackFunction) {
-                    callbackFunction(...param) // Chama a função de callback, se existir
+                    callbackFunction(...param)
                 }
-                let dados = await buscarDados('tipos_entrada'); // Busca os dados da tabela, exibe na tela e permite pesquisar
-                carregarDadosNaTabela(dados, ["id_tipo_de_entrada", "descricao", "cfop_dentro","cfop_fora","ativo"]) // Exibe na tela e permite pesquisar
             }
         } catch (err) {
             popup_carregando(true)
